d1557609a86bbc8b3addf272237f473f743e340f
wangkai@ACER <wangkai001_tg@163.com>
2016-06-19 17:06:23 -0700

    changegray

--- Python-Vision/1.1/1.1.2/slt.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-from pylab import *
-
-pil_im = Image.open('../../IMAGES/B.jpg')
-test = pil_im.thumbnail((128,128))
-print test
-imshow(pil_im)
-show()
--- Python-Vision/libsvm_using.py
+++
@@ -1,7 +0,0 @@
-#coding:utf-8
-import pickle
-from svmutil import *
-
-#us
-with open('points_normal.pkl','r') as f:
-    class_1 = pickle.load(f)
